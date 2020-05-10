c90e24ea3c8ca04fb89f4d9e900aa80535fafcbd
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-05-10 04:33:02 +0100

    positive chat

---
+++ src/components/LineChat.vue
@@ -0,0 +1,44 @@
+<script>
+import { Line } from "vue-chartjs";
+export default {
+  extends: Line,
+  props: {
+    label: {
+      type: String
+    },
+    chartData: {
+      type: Array
+    },
+    options: {
+      type: Object
+    },
+    chartColors: {
+      type: Object
+    }
+  },
+  mounted() {
+    const dates = this.chartData.map(d => d.date).reverse();
+    const totals = this.chartData.map(d => d.total).reverse();
+    const {
+      borderColor,
+      pointBackgroundColor,
+      backgroudColor
+    } = this.chartColors;
+    this.renderChart(
+      {
+        labels: dates,
+        datasets: [
+          {
+            label: this.label,
+            data: totals,
+            borderColor: borderColor,
+            pointBackgroundColor: pointBackgroundColor,
+            backgroudColor: backgroudColor
+          }
+        ]
+      },
+      this.options
+    );
+  }
+};
+</script>
